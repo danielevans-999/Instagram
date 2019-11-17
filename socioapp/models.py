9f7d537030f5331bdf66bf0e4be0379b7bf12f8c
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-17 11:53:43 +0300

    Adds two models the Image and the Profile Models

--- socioapp/models.py
+++ socioapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=50)
+    image_caption = models.CharField(max_length=50)
+    profile = models.ForeignKey('Profile',on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    comments = models.CharField(max_length=50)
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+        
+
+    
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='images')
+    bio = models.TextField()
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+        
+    def update_bio(self):
+        pass
