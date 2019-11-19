1e915396fac20c3ee484f5c8163300dcd4885e85
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-19 15:57:09 +0300

    Imports the votemodels and inherit to the Image Models

--- socioapp/models.py
+++ socioapp/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from vote.models import VoteModel
+from vote.managers import VotableManager
 
-class Image(models.Model):
+class Image(VoteModel,models.Model):
     image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=50)
     image_caption = models.CharField(max_length=50)
-    likes = models.IntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     
     def save_image(self):
